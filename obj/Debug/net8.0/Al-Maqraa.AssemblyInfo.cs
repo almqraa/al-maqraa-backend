a95cba8fe8c5184624bf74d5879a7e6b57b02d9b
Mohamed Gamal <mohamedgamal45031@gmail.com>
2024-06-29 15:58:23 +0300

    hamada

--- obj/Debug/net8.0/Al-Maqraa.AssemblyInfo.cs
+++ obj/Debug/net8.0/Al-Maqraa.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Al-Maqraa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+549d898124693c7687fdf9b214458b15ed5e90f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3272f2d5b21f3a6bbc61d336323bcf2014174e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Al-Maqraa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Al-Maqraa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
