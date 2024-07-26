50f0a30b42351871321b67cd9b6ad059a19ad9de
Omar Walid <om.walid2003@gmail.com>
2024-07-26 18:56:11 +0300

    change home page

--- obj/Debug/net8.0/Al-Maqraa.AssemblyInfo.cs
+++ obj/Debug/net8.0/Al-Maqraa.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Al-Maqraa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d23a56302c7d84783d49f85ada0ff503af4de4c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a04fc13b1a8c9a6a326831d80e2508116c97e67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Al-Maqraa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Al-Maqraa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
